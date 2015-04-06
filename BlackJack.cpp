727964634d3628f4b0449c61977e5f505e9bae8b
sunyifu <mail.sjtu.edu.cn>
2015-04-06 22:35:05 +0800

    This is a C++ program written by sunyifu.
    This program is a solution to BlackJack.

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -3,25 +3,25 @@
 #include <iostream>
 using namespace std;
 
-void shuffle(int num[ ], int flag[ ]);				// shuffle;
-double player_s_turn(double sum1, int num[ ]);				// player's turn to deal;					
-double deal(double sum, int num[ ], int k);				// deal;
-double check(int num[ ], int k);				// check;
+void shuffle(int card[ ]);				// shuffle;
+double player_s_turn(double sum1, int card[ ]);				// player's turn to deal;					
+double deal(double sum, int card[ ], int k);				// deal;
+double check(int card[ ], int k);				// check;
 char check_input(char request);				// check the input;
-double computer_s_turn(double sum2, int num[ ], double sum1);				// computer's turn to deal;
+double computer_s_turn(double sum2, int card[ ], double sum1);				// computer's turn to deal;
 
 int i = 0, j = 0;				// we count the number of cards by using two global variables i, j;
-								// however, this may lead to unexpected changes to their value;							
-int main()						// i tried to construct a function which returns several value, yet failed, you can improve it;
+								// however, this may lead to unexpected changes to their values;							
+int main()						// i tried to construct a function which returns several values, yet failed, you can improve it;
 {					
-	int num[52], flag[52] = {0};				// each number represents a card;
+	int card[52];				// each number represents a card;
 	double sum1 = 0, sum2 = 0;					// sum1, sum2 count the sum of cards in player's hands and computer's hands respectively;
 	char request;
 
-	shuffle(num, flag);
-	sum1 = player_s_turn(sum1, num);
+	shuffle(card);
+	sum1 = player_s_turn(sum1, card);
 	if (sum1 > 21) cout << endl << "You lose" << endl;
-	else sum2 = computer_s_turn(sum2, num, sum1);
+	else sum2 = computer_s_turn(sum2, card, sum1);
 
 	cout << endl << endl << "Play again(y/n): ";
 
@@ -34,18 +34,22 @@ int main()						// i tried to construct a function which returns several value, 
 	return 0;
 }
 
-void shuffle(int num[ ], int flag[ ])
+void shuffle(int card[ ])
 {
+	int num[52], randNum;
+
 	srand(time(NULL));
 
+	for (int k = 0; k < 52; ++k) num[k] = k;
 	for (int k = 0; k < 52; ++k) {
-		do {
-			num[k] = rand() % 52;
-		} while (flag[num[k]] != 0);				
-		++flag[num[k]];			 }				// the same card should not repeat twice;
+		randNum = rand() % (52 - k);
+		card[k] = num[randNum];
+		num[randNum] = num[51 - k];}				// the same card should not repeat twice;
+	
+	system("pause");
 }
 
-double player_s_turn(double sum1, int num[ ])
+double player_s_turn(double sum1, int card[ ])
 {	
 	char request;
 
@@ -53,7 +57,7 @@ double player_s_turn(double sum1, int num[ ])
 	cout << "Cards in player's hands: ";
 
 	for (i = 0; i < 2; ++i)
-		sum1 = deal(sum1, num, i); 
+		sum1 = deal(sum1, card, i); 
 
 	cout << endl << endl << "Do you want another deal(y/n): ";
 
@@ -61,8 +65,8 @@ double player_s_turn(double sum1, int num[ ])
 	while (request == 'y' || request == 'Y') {
 		cout << endl << "Another deal: ";
 		for (int k = 0; k < i; ++k)
-			check(num, k);		
-		sum1 = deal(sum1, num, i++);
+			check(card, k);		
+		sum1 = deal(sum1, card, i++);
 		if (sum1 > 21) break;
 		else {
 			cout << endl << endl << "Do you want another deal(y/n): ";
@@ -71,24 +75,24 @@ double player_s_turn(double sum1, int num[ ])
 	return sum1;
 }
 
-double deal(double sum, int num[ ], int k)
+double deal(double sum, int card[ ], int k)
 {	
-	sum += check(num, k);
+	sum += check(card, k);
 
 	return sum;
 }
 
-double check(int num[ ], int k)
+double check(int card[ ], int k)
 {	
 	double value[52];
 
-	switch (num[k] / 13) {
+	switch (card[k] / 13) {
 	case 0: cout << "spades"; break;
 	case 1: cout << "hearts"; break;
 	case 2: cout << "clubs"; break;
 	case 3: cout << "diamonds"; break;
 	}
-	switch (num[k] % 13) {
+	switch (card[k] % 13) {
 	case 0: cout << " Ace "; value[k] = 1; break;
 	case 1: cout << " two "; value[k] = 2; break;
 	case 2: cout << " three "; value[k] = 3; break;
@@ -120,18 +124,18 @@ char check_input(char request)
 	return request;
 }
 
-double computer_s_turn(double sum2, int num[ ], double sum1)
+double computer_s_turn(double sum2, int card[ ], double sum1)
 {
 	cout << endl << endl << "Computer's turn to deal" << endl;
 	cout << "Cards in computer's hands: ";
 
 	for (j = 0; j < 2; ++j)
-		sum2 = deal(sum2, num, i + j);
+		sum2 = deal(sum2, card, i + j);
 	while (sum2 < sum1 || sum2 == sum1 && j < i) {
 	cout << endl << "Another deal: ";
 	for (int k = 0; k < j; ++k)
-		check(num, k + i);		
-	sum2 = deal(sum2, num, i + j++);			 }
+		check(card, k + i);		
+	sum2 = deal(sum2, card, i + j++);			 }
 	if (sum2 > 21) {
 		cout << endl << endl << "You win" << endl;}
 	else if (sum2 == sum1 && j == i) {
